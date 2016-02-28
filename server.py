4613d236ebf25c72c8de6c5c11876bea86814bf4
Skyler Kidd <f.s.kidd@gmail.com>
2016-02-27 23:14:04 -0800

    Testing if everything is still broken

--- server.py
+++ server.py
@@ -1,29 +1,10 @@
-from twilio.rest import TwilioRestClient
 from flask import Flask, jsonify, request
 import os
 
-if 'ACCOUNT_SID' in os.environ:
-	ACCOUNT_SID = os.environ['ACCOUNT_SID']
-	AUTH_TOKEN = os.environ['AUTH_TOKEN']
-	password = os.environ['password']
-	toNum = os.environ["toNum"],
-	fromNum = os.environ["fromNum"],
-else:
-	config = {}
-	execfile("settings.conf", config)
-	ACCOUNT_SID = config["ACCOUNT_SID"]
-	AUTH_TOKEN = config["AUTH_TOKEN"]
-	password = config['password']
-	toNum = config["toNum"],
-	fromNum = config["fromNum"],
-
-client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 
 app = Flask(__name__)
 
 #--------------------
-
-
 
 @app.route('/api/v1.0/alert', methods=['POST'])
 def alert_me():

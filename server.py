ba89064aec646b374abc5424052ba889d875ba7e
Skyler Kidd <f.s.kidd@gmail.com>
2016-02-27 23:16:32 -0800

    Back to the way things were. Before the dark times.

--- server.py
+++ server.py
@@ -1,15 +1,46 @@
+from twilio.rest import TwilioRestClient
 from flask import Flask, jsonify, request
 import os
 
+if 'ACCOUNT_SID' in os.environ:
+	ACCOUNT_SID = os.environ['ACCOUNT_SID']
+	AUTH_TOKEN = os.environ['AUTH_TOKEN']
+	password = os.environ['password']
+	toNum = os.environ["toNum"],
+	fromNum = os.environ["fromNum"],
+else:
+	config = {}
+	execfile("settings.conf", config)
+	ACCOUNT_SID = config["ACCOUNT_SID"]
+	AUTH_TOKEN = config["AUTH_TOKEN"]
+	password = config['password']
+	toNum = config["toNum"],
+	fromNum = config["fromNum"],
+
+client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 
 app = Flask(__name__)
 
 #--------------------
 
+
+
 @app.route('/api/v1.0/alert', methods=['POST'])
 def alert_me():
-	resp = 1
-	return resp
+    if not request.json or not 'title' in request.json:
+        abort(400)
+	if request.json.password != password:
+		abort(400)
+
+	client.messages.create(
+		to = toNum,
+		from_ = fromNum,
+		body = "Alert!",
+	)
+
+	print "Message sent!"
+
+    return jsonify({'resp':{'title': request.json['title'],'status': True}})
 
 	#--------------------
 

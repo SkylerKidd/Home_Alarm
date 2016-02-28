64c8b8cee4ea13ef5d143e5b5b83c11e97b534e8
Skyler Kidd <f.s.kidd@gmail.com>
2016-02-27 20:46:59 -0800

    http POST call

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 from twilio.rest import TwilioRestClient
-
-# put your own credentials here
+from flask import Flask, jsonify
 
 config = {}
 execfile("settings.conf", config)
@@ -10,10 +9,39 @@ AUTH_TOKEN = config["AUTH_TOKEN"]
 
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 
-client.messages.create(
-	to=config["to"],
-	from_=config["from"],
-	body="Alert!",
-)
+app = Flask(__name__)
+
+#--------------------
+
+@app.route('/api/v1.0/alert', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+	if request.json.password != config["password"]:
+		abort(400)
+
+	client.messages.create(
+		to=config["to"],
+		from_=config["from"],
+		body="Alert!",
+	)
+
+	resp = {
+		'title': request.json['title'],
+		'status': True
+	}
+
+    return jsonify({'resp': resp}), 201
+
+#--------------------
 
 print "Message sent!"
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
